dcae99385261f90a729e55747b017ff6ab31b1c5
Krishna Jaiswal <143750524+Krishnajais347@users.noreply.github.com>
2026-05-11 04:02:46 +0530

    al goog

---
+++ break-fun.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int n = 8,t;
+    for (int i = 0; i < n; i++)
+    {
+        if (i == 5)
+        {t=i;
+            break;
+        }
+    }
+    cout << t;
+}
