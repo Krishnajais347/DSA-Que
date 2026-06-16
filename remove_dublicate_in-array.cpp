ab781cc18273247a0946f9d89c09a0d65888cc79
Krishna Jaiswal <krishnajaiswal220417@acropolis.in>
2026-06-17 00:50:14 +0530

    first_dublicate in an array

--- remove_dublicate_in-array.cpp
+++ remove_dublicate_in-array.cpp
@@ -5,16 +5,16 @@ void remove_dublicate(int arr[], int length)
 {
     for (int i = 0; i < length; i++)
     {
-        int tmp = 0;
         for (int j = i + 1; j < length; j++)
         {
             if (arr[i] == arr[j])
             {
-                for (int k = j; k < length; k++)
+                for (int k = j; k < length - 1; k++)
                 {
-                    arr[j] = arr[j + 1];
+                    arr[k] = arr[k + 1];
                 }
                 length--;
+                j--;
             }
         }
     }
@@ -22,7 +22,7 @@ void remove_dublicate(int arr[], int length)
     cout << "the currect array is : - ";
     for (int i = 0; i < length; i++)
     {
-        cout << arr[i]<<" ";
+        cout << arr[i] << " ";
     }
 }
 int main()
@@ -30,7 +30,7 @@ int main()
     int n;
     // cout << "enter the  element that ypu want to. search in an array\n";
     // cin >> n;
-    int arr[] = {43, 100, 57, 78, 100, 45, 67, 43, 57, 100};
+    int arr[] = {43, 43, 57, 78, 100, 45, 67, 43, 57, 100};
     int length = sizeof(arr) / sizeof(arr[0]);
     cout << "the array is : - ";
     for (int i = 0; i < length; i++)
